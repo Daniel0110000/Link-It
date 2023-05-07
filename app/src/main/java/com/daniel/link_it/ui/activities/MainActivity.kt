23dc447b77c67b541b21a314b1d4676fb35b8ce0
Huvert01 <lupi2102casrillo@gmail.com>
2023-05-07 17:36:14 -0600

    build: LoginDesign Update

--- app/src/main/java/com/daniel/link_it/ui/activities/MainActivity.kt
+++ app/src/main/java/com/daniel/link_it/ui/activities/MainActivity.kt
@@ -7,6 +7,6 @@ import com.daniel.link_it.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
