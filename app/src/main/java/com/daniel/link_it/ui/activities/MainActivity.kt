d843cd2329a23a2387635b142f0e5b1fde2612d5
Daniel <cariasdaniel261@gmail.com>
2023-05-07 16:51:44 -0600

    build: First commit

---
+++ app/src/main/java/com/daniel/link_it/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.daniel.link_it.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.daniel.link_it.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
