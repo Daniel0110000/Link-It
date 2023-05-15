cf2bdc549873f7da8adf9d93e89cd1a99f81c2b8
Huvert01 <lupi2102casrillo@gmail.com>
2023-05-14 18:23:45 -0600

    build: functionalities(login, sendMsg)

--- app/src/main/java/com/daniel/link_it/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/daniel/link_it/ui/activities/LoginActivity.kt
@@ -1,12 +1,74 @@
 package com.daniel.link_it.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.daniel.link_it.R
+import org.json.JSONObject
+
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val sharedPref = getSharedPreferences("linkitPrefs", Context.MODE_PRIVATE)
+        val requestQueue = Volley.newRequestQueue(this)
+        fun reqPostLogin(url: String, json: JSONObject) {
+            val solicitud = JsonObjectRequest(
+                Request.Method.POST, url, json,
+                Response.Listener { response ->
+                    // Manejar la respuesta de la petición
+                    val status = response.getString("status")
+                    Toast.makeText(applicationContext,status,Toast.LENGTH_SHORT).show()
+                    if (status.equals("200")){
+                        Toast.makeText(this, "TOdo bem", Toast.LENGTH_SHORT).show()
+                        val editor = sharedPref.edit()
+                        editor.putString("username", json.getString("username"))
+                        editor.putString("password", json.getString("password"))
+                        editor.apply()
+                    }else{
+                        Toast.makeText(this, "Credenciales incorretas", Toast.LENGTH_SHORT).show()
+                    }
+                },
+                Response.ErrorListener { error ->
+                    // Manejar el fallo de la petición
+                    Toast.makeText(applicationContext,error.toString(), Toast.LENGTH_SHORT).show()
+                    print(error.toString())
+                })
+
+            requestQueue.add(solicitud)
+        }
+        val hasSesion = sharedPref.getString("username", null)
+        if(hasSesion != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        Toast.makeText(this,hasSesion.toString(), Toast.LENGTH_SHORT).show()
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener{
+
+            val username = findViewById<EditText>(R.id.username)
+            val password = findViewById<EditText>(R.id.password)
+            val chLogin = findViewById<CheckBox>(R.id.chLogin)
+            val url = "https://link-it-production.up.railway.app/test"
+            val json = JSONObject()
+            json.put("username", username.text)
+            json.put("password", password.text)
+            json.put("chosed_login", chLogin.isChecked)
+            json.put("java", true)
+            Toast.makeText(applicationContext,json.toString(), Toast.LENGTH_SHORT).show()
+            reqPostLogin(url, json)
+        }
     }
 }
--- app/src/main/java/com/daniel/link_it/ui/activities/MainActivity.kt
+++ app/src/main/java/com/daniel/link_it/ui/activities/MainActivity.kt
@@ -1,12 +1,102 @@
 package com.daniel.link_it.ui.activities
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import com.daniel.link_it.R
+// Socket.IO
+import io.socket.client.IO
+import io.socket.client.Socket
+import java.net.URISyntaxException
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import org.json.JSONObject
+import org.json.JSONArray
+import android.view.ViewGroup.MarginLayoutParams
 
 class MainActivity : AppCompatActivity() {
+    object SocketHandler {
+        lateinit var mSocket: Socket
+        @Synchronized
+        fun setSocket() {
+            try {
+                mSocket = IO.socket("https://link-it-production.up.railway.app")
+            } catch (e: URISyntaxException) {
+
+            }
+        }
+        @Synchronized
+        fun getSocket(): Socket {
+            return mSocket
+        }
+        @Synchronized
+        fun establishConnection() {
+            mSocket.connect()
+        }
+        @Synchronized
+        fun closeConnection() {
+            mSocket.disconnect()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
+
+        val sharedPref = getSharedPreferences("linkitPrefs", Context.MODE_PRIVATE)
+        val username = sharedPref.getString("username", null)
+        SocketHandler.setSocket()
+        SocketHandler.establishConnection()
+        val mSocket = SocketHandler.getSocket()
+        val inputMsg = findViewById<EditText>(R.id.inputMsg)
+        val btnSnd = findViewById<Button>(R.id.btnSnd)
+
+        Toast.makeText(this,username, Toast.LENGTH_SHORT).show()
+        val userData = JSONObject()
+        userData.put("user", username)
+        mSocket.emit("getUser",userData)
+
+        // args[0] is the data from the server
+        // Change "as Int" according to the data type
+        // Example "as String" or write nothing
+        // Logging the data is optional
+        val msgContainer = findViewById<LinearLayout>(R.id.msgCont)
+        mSocket.on("getMessages") { args ->
+            if (args[0] != null) {
+                val msgsJSONArray = args[0] as JSONArray
+                runOnUiThread {
+                    for(i in 0 until msgsJSONArray.length()){
+                        val tv = TextView(this)
+                        tv.setTextColor(Color.parseColor("#FFFFFF"))
+                        val backgroundDrawable = resources.getDrawable(R.drawable.backgroundradious)
+                        tv.background = backgroundDrawable
+                        tv.setPadding(35,20,35,20)
+                        val layoutParams = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT
+                        )
+                        layoutParams.setMargins(10, 20, 10, 20)
+                        tv.layoutParams = layoutParams
+                        tv.text = msgsJSONArray.getJSONObject(i).getString("message")
+                        msgContainer.addView(tv)
+                    }
+                }
+            }
+        }
+        btnSnd.setOnClickListener{
+            val msgData = JSONObject()
+            msgData.put("message", inputMsg.text)
+            msgData.put("user", username)
+            msgData.put("folder", "main")
+            Toast.makeText(this,msgData.toString(), Toast.LENGTH_SHORT).show()
+            mSocket.emit("chat:message", msgData)
+        }
     }
 }
